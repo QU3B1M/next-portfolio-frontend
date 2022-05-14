d9c50eb34560e99103e73f4592881892e22fb299
QU3B1M <kevinledesmam95@gmail.com>
2022-05-14 20:16:40 -0300

    Added Navbar

---
+++ pages/projects.tsx
@@ -0,0 +1,8 @@
+const projects = () => {
+  return (
+	<div className="p-4">
+    <h1>Projects</h1>
+	</div>
+  );
+};
+export default projects;
---
+++ pages/resume.tsx
@@ -0,0 +1,8 @@
+const resume = () => {
+  return (
+      <div className="p-4">
+        Resume
+      </div>
+  );
+}
+export default resume;
